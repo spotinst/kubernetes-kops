21e40f0717fe3a8a5062700af7b52ef7f152b43d
liranp <liran.polak@gmail.com>
2020-12-15 16:08:23 +0200

    chore: cherry-picked changes from v1.20

--- upup/models/bindata.go
+++ upup/models/bindata.go
@@ -19998,6 +19998,18 @@ rules:
 - apiGroups: [""]
   resources: ["pods"]
   verbs: ["get", "list", "patch", "update", "create", "delete"]
+- apiGroups: ["batch"]
+  resources: ["jobs"]
+  verbs: ["get", "list", "patch","update","create","delete"]
+  # ----------------------------------------------------------------------------
+  # Required by Spotinst Wave.
+  # ----------------------------------------------------------------------------
+- apiGroups: ["sparkoperator.k8s.io"]
+  resources: ["sparkapplications", "scheduledsparkapplications"]
+  verbs: ["get", "list"]
+- apiGroups: ["wave.spot.io"]
+  resources: ["sparkapplications", "wavecomponents", "waveenvironments"]
+  verbs: ["get", "list"]
 ---
 # ------------------------------------------------------------------------------
 # Cluster Role Binding
@@ -20039,6 +20051,13 @@ spec:
       priorityClassName: system-cluster-critical
       affinity:
         nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: kubernetes.io/os
+                operator: NotIn
+                values:
+                - windows
           preferredDuringSchedulingIgnoredDuringExecution:
           - weight: 100
             preference:
@@ -20059,7 +20078,7 @@ spec:
       containers:
       - name: spotinst-kubernetes-cluster-controller
         imagePullPolicy: Always
-        image: spotinst/kubernetes-cluster-controller:1.0.67
+        image: spotinst/kubernetes-cluster-controller:1.0.72
         livenessProbe:
           httpGet:
             path: /healthcheck
@@ -20145,6 +20164,7 @@ spec:
             fieldRef:
               fieldPath: metadata.namespace
       serviceAccountName: spotinst-kubernetes-cluster-controller
+      dnsPolicy: Default
       tolerations:
       - key: node.kubernetes.io/not-ready
         effect: NoExecute
